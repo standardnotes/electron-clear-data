c63b5061ed62c03d98ef5c2d460fb34e8761b1c1
Johnny Almonte <johnny243@users.noreply.github.com>
2021-03-25 02:17:06 -0400

    fix: relaunch app when clearing user data

---
+++ main.d.ts
@@ -0,0 +1,7 @@
+declare module 'electron-clear-data' {
+  /**
+ * Clears all user data.
+ * The user data directory is recreated after re-launching the application.
+ */
+  export function clearUserData(): void
+}
--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 import { app } from 'electron';
 import fs from 'fs-extra';
-import { join } from 'path';
 
 /**
  * Clears all user data.
- * The user data directory is recreated by Electron automatically.
+ * The user data directory is recreated after re-launching the application.
  */
-export const clearUserData = () => {
+export function clearUserData () {
     const userDataPath = app.getPath('userData');
-    fs.unlink(userDataPath);
-};
-
-/**
- * Clears all app data.
- * The app data directory is recreated by Electron automatically.
- */
-export const clearAppData = () => {
-    const appName = app.getName();
-    const basePath = app.getPath('appData');
-    const appDataPath = join(basePath, appName);
-    fs.unlink(appDataPath);
+    fs.removeSync(userDataPath);
+    app.relaunch();
+    app.exit();
 };
