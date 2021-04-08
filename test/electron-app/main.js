dccdcd433696c0168a560b98290df7ff192afaa8
Johnny Almonte <johnny243@users.noreply.github.com>
2021-04-08 17:00:04 -0400

    fix: change look of the test electron application

--- test/electron-app/main.js
+++ test/electron-app/main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
-    height: 300,
-    width: 400,
+    height: 200,
+    width: 600,
     title: 'Electron App',
     webPreferences: {
       devTools: false
