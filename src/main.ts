62b75ea8f96773f4bce10988efdf9deb1e63d4a0
Johnny Almonte <johnny243@users.noreply.github.com>
2021-05-08 21:18:38 -0400

    fix: use session.clearStorageData()

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
-import { app } from 'electron';
-import { emptyDirSync } from 'fs-extra';
-import path from 'path';
+import { app, BrowserWindow } from 'electron';
 
 const relaunchApp = () => {
   app.relaunch();
   app.exit();
 };
 
+const getFocusedWindow = () => {
+  return BrowserWindow.getFocusedWindow() ?? BrowserWindow.getAllWindows()[0];
+};
+
 /**
  * Removes the contents of the user data directory.
  * This directory is then recreated after re-launching the application.
  */
-export function clearUserDataDirectory(): void {
-  const userDataPath = app.getPath('userData');
-  emptyDirSync(userDataPath);
+export function clearAllUserData(): void {
+  const { webContents } = getFocusedWindow();
+  webContents.session.flushStorageData();
+  webContents.session.clearStorageData();
   relaunchApp();
 };
 
@@ -22,19 +25,14 @@ export function clearUserDataDirectory(): void {
  * Each directory is reinitialized after re-launching the application.
  */
 export function clearSensitiveDirectories(): void {
-  const userDataPath = app.getPath('userData');
-  /**
-   * A list of directories that contain leveldb databases.
-   * All data is wiped along the *.log and *.ldb files.
-   */
-  const directoriesToRemove = [
-    'Local Storage',
-    'IndexedDB',
-    'Session Storage'
-  ];
-  directoriesToRemove.forEach((item) => {
-    const removeDirectory = path.join(userDataPath, item);
-    emptyDirSync(removeDirectory);
+  const { webContents } = getFocusedWindow();
+  webContents.session.flushStorageData();
+  webContents.session.clearStorageData({
+    storages: [
+      'indexdb',
+      'localstorage',
+      'websql'
+    ]
   });
   relaunchApp();
 };
